b90744c7bd40b43c08a4d9ee861c2da3fd2b860b
LAPTOP-6TDP5TMD\rodri <rodrigomfl@hotmail.com>
2020-05-25 12:30:50 +0100

    added validation to executable in execution service, removed unused imports

--- Code/services/src/main/kotlin/pt/iselearning/services/controller/ExecutionController.kt
+++ Code/services/src/main/kotlin/pt/iselearning/services/controller/ExecutionController.kt
@@ -1,16 +1,10 @@
 package pt.iselearning.services.controller
 
-import org.springframework.http.MediaType.APPLICATION_JSON
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
-import org.springframework.web.util.UriComponentsBuilder
-import pt.iselearning.services.domain.Challenge
 import pt.iselearning.services.domain.Executable
 import pt.iselearning.services.domain.ExecutableResult
-import pt.iselearning.services.service.ChallengeAnswerService
-import pt.iselearning.services.service.ChallengeService
 import pt.iselearning.services.service.ExecutionService
-import java.lang.String
 
 /**
  * Handler responsible to respond to requests regarding code execution
--- Code/services/src/main/kotlin/pt/iselearning/services/domain/Executable.kt
+++ Code/services/src/main/kotlin/pt/iselearning/services/domain/Executable.kt
@@ -1,8 +1,11 @@
 package pt.iselearning.services.domain
 
+import org.springframework.validation.annotation.Validated
+
 /**
 * data class that represents the result of an execution
 */
+@Validated
 data class Executable(
         var language : String?,
         var code : String?,
