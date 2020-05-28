f4fcf1d60a38e9eebdbba22199dd4176c26f79e9
LAPTOP-6TDP5TMD\rodri <rodrigomfl@hotmail.com>
2020-05-28 13:38:15 +0100

    remove commente code

--- Code/services/src/main/kotlin/pt/iselearning/services/domain/ChallengeTag.kt
+++ Code/services/src/main/kotlin/pt/iselearning/services/domain/ChallengeTag.kt
@@ -16,13 +16,6 @@ data class ChallengeTag (
     @Column(name="challenge_id")
     var challengeId : Int? =  null,
 
-    /*
-    @Column(name="tag")
-    var tag : String? =  null
-    @ManyToOne(cascade = [CascadeType.ALL])
-    @JoinColumn(name="challenge_id", nullable=false, updatable=false, insertable=false)
-    var challenge : Challenge?,*/
-
     @ManyToOne(cascade = [CascadeType.ALL])
     @JoinColumn(name="tag_id", nullable=false, updatable=false, insertable=true)
     var tag : Tag?
--- Code/services/src/main/kotlin/pt/iselearning/services/domain/Tag.kt
+++ Code/services/src/main/kotlin/pt/iselearning/services/domain/Tag.kt
@@ -15,20 +15,6 @@ data class Tag (
 
     @Column(name="tag")
     var tag : String?
-
-        /*
-        @OneToMany
-        @JoinColumn(name="tag_id", nullable=false, updatable=false, insertable=false)
-        var challengeTag: List<ChallengeTag>?
-
-            @ManyToMany(fetch = FetchType.EAGER, cascade = [CascadeType.ALL], mappedBy = "tags")
-            @JoinTable(
-                name = "ct",
-                joinColumns = [JoinColumn(name = "tag")],
-                inverseJoinColumns =[JoinColumn(name = "challenge_id")]
-            )*/
-    /*@OneToMany(mappedBy = "challenge")
-    var challenges : List<ChallengeTag>?*/
 ) {
     constructor() : this(null, null)
 }
