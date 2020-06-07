eadc697b7e8bb55c5cec04f6c8caf6920615ee4c
jerss <a44847@alunos.isel.pt>
2020-06-07 12:59:26 +0100

    Update Challenge.kt

--- Code/services/src/main/kotlin/pt/iselearning/services/domain/Challenge.kt
+++ Code/services/src/main/kotlin/pt/iselearning/services/domain/Challenge.kt
@@ -30,15 +30,6 @@ data class Challenge (
     @OneToMany(cascade = [CascadeType.ALL], targetEntity=Solution::class)
     @JoinColumn(name="challenge_id", nullable=false, updatable=true, insertable=true)
     var solutions : MutableList<Solution>?
-
-    /*@ManyToMany(fetch = FetchType.EAGER, cascade = [CascadeType.ALL])
-    @JoinTable(
-            name = "ct",
-            joinColumns = [JoinColumn(name = "challenge_id")],
-            inverseJoinColumns =[JoinColumn(name = "tag")]
-    )*/
-    //@OneToMany(mappedBy = "tag")
-    //var tags : List<ChallengeTag>?
 ) {
     constructor() : this(null, null, null, null, null)
 }
