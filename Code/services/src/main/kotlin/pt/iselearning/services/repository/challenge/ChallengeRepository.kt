b6681af31e11c080b235b2e143f508f9f5a6e67f
LAPTOP-24HRLEIC\Quintela <aquintelaoliveira@gmail.com>
2020-08-22 18:27:10 +0100

    get random challenge service and controller

--- Code/services/src/main/kotlin/pt/iselearning/services/repository/challenge/ChallengeRepository.kt
+++ Code/services/src/main/kotlin/pt/iselearning/services/repository/challenge/ChallengeRepository.kt
@@ -1,5 +1,6 @@
 package pt.iselearning.services.repository.challenge
 
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 import pt.iselearning.services.domain.challenge.Challenge
@@ -12,4 +13,6 @@ interface ChallengeRepository : CrudRepository<Challenge, Int> {
     fun findAllByCreatorId(userId: Int): List<Challenge>
     fun findAllByIsPrivate(isPrivate : Boolean): List<Challenge>
     fun findAllByCreatorIdAndIsPrivate(userId: Int, private: Boolean): List<Challenge>
+    @Query(value = "SELECT c.challengeId FROM Challenge c")
+    fun findAllChallengeIds(): List<Int>
 }
